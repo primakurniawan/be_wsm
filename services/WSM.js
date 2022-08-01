33f6432198fb73c29d534e087773b560b5685320
Prima Tondy Arya Kurniawan <primakurniawan666@gmail.com>
2022-08-02 01:18:19 +0700

    fix wsm

--- services/WSM.js
+++ services/WSM.js
@@ -8,20 +8,28 @@ function WSM(alternatifArray, subkriteriaCalon) {
 
   alternatifArray.forEach((alternatif) => {
     let skor = 0;
+    let memenuhiSyarat = true;
     alternatif.kriteriaArray.forEach((kriteriaAlternatif) => {
       kriteriaAlternatif.subkriteriaArray.forEach((subkriteriaAlternatif) => {
         subkriteriaCalon.forEach((subkriteriaCalon) => {
           if (subkriteriaAlternatif.id_subkriteria == subkriteriaCalon.id_subkriteria) {
             subkriteriaAlternatif.nilai_bobot = subkriteriaCalon.bobot * subkriteriaAlternatif.nilai;
             skor += subkriteriaAlternatif.nilai_bobot;
+            if (subkriteriaAlternatif.nilai > subkriteriaCalon.nilai) {
+              memenuhiSyarat = false;
+              console.log("subkriteriaAlternatif.nilai", subkriteriaAlternatif.nilai);
+              console.log("subkriteriaCalon.nilai", subkriteriaCalon.nilai);
+            }
           }
         });
       });
       alternatif.skor = skor;
+      alternatif.memenuhiSyarat = memenuhiSyarat;
     });
   });
 
   const rank = alternatifArray
+    .filter((e) => e.memenuhiSyarat === true)
     .sort((a, b) => b.skor - a.skor)
     .map((e, i) => {
       return {
